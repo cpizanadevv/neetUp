d61f36b20995c81b255d472d816b565ff12be6be
cj <c.pizana.devv@gmail.com>
2024-03-27 15:28:33 -0700

    after break commit, added security middleware

---
+++ backend/app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+require("express-async-errors");
+const morgan = require("morgan");
+const cors = require("cors");
+const csurf = require("csurf");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+
+const { environment } = require("./config");
+const isProduction = environment === "production";
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(cookieParser());
+app.use(express.json());
+
+//*  Security Middleware
+
+    // only enables cors in development
+if (!isProduction) app.use(cors());
+
+    //helps set various headers to better secure the app
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin",
+  })
+);
+
+    // Set the _csrf token and create req.csrfToken method
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true,
+    },
+  })
+);
---
+++ backend/config/database.js
@@ -0,0 +1,28 @@
+// backend/config/database.js
+// Allows db to load config environment vars from .env into config/index.js
+// Defines global schema for proj
+const config = require('./index');
+
+module.exports = {
+  development: {
+    storage: config.dbFile,
+    dialect: "sqlite",
+    seederStorage: "sequelize",
+    logQueryParameters: true,
+    typeValidation: true
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    define: {
+      schema: process.env.SCHEMA
+    }
+  }
+};
