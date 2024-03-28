94c9c5bb44b5b5f2579674f3d85294bf77e76a69
cj <c.pizana.devv@gmail.com>
2024-03-28 13:34:13 -0700

    Added scope to User model, for user security

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -52,6 +52,11 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+    defaultScope: {
+      attributes: {
+        exclude: ['hashedPassword', 'email', 'updatedAt', 'createdAt'],
+      }
+    }
   });
   return User;
 };
