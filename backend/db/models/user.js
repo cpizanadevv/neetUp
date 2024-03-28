a0db064fa79a388b76f567e47dadd8301b42e095
cj <c.pizana.devv@gmail.com>
2024-03-28 13:20:23 -0700

    Added Users table, migration, and tester seeds

---
+++ backend/db/models/user.js
@@ -0,0 +1,57 @@
+'use strict';
+const {
+  Model, Validator
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: {
+          args: [4,30],
+          msg: "Username must be between 4 and 30 characters."
+        },
+        isNotEmail(value){
+          if(Validator.isEmail(value)){
+            throw new Error("Email cannot be used as username.")
+          }
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: {
+          args: [3,256],
+          msg: "Email must be between 3 and 256 characters."
+        },
+        isEmail: true,
+      }
+    },
+    hashedPassword: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [60,60],
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
