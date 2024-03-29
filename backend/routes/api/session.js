a1ebf8591becb2cf96714dd99bbe9ee6db3a7ed0
cj <c.pizana.devv@gmail.com>
2024-03-29 16:09:57 -0700

    Added backend endpoint to get curr user session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -7,6 +7,19 @@ const { User } = require("../../db/models");
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    const { user } = req;
+    if(user) {
+        const safeUser = {
+            id: user.id,
+            email: user.email,
+            username: user.uername
+        };
+        return res.json({
+            user: safeUser
+        });
+    }else return res.json({ user: null });
+});
 
 router.post("/", async (req, res, next) => {
   const { credential, password } = req.body;
