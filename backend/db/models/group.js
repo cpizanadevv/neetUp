085c827a8e4c8ed024c8d6262b44870941ce44a8
cj <c.pizana.devv@gmail.com>
2024-05-15 12:33:24 -0700

    Finished find all groups from current user

--- backend/db/models/group.js
+++ backend/db/models/group.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Group extends Model {
     /**
@@ -11,93 +9,144 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Group.hasMany(models.GroupImage,{ foreignKey: 'groupId'});
-      Group.hasMany(models.Venue,{ foreignKey: 'groupId'});
-      Group.hasMany(models.Event, { foreignKey: 'groupId'});
-      Group.hasMany(models.Membership, { foreignKey: 'groupId'});
-      Group.belongsTo(models.User, { foreignKey: 'organizerId' });
+      Group.hasMany(models.GroupImage, { foreignKey: "groupId" });
+      Group.hasMany(models.Venue, { foreignKey: "groupId" });
+      Group.hasMany(models.Event, { foreignKey: "groupId" });
+      Group.hasMany(models.Membership, { foreignKey: "groupId" });
+      Group.belongsTo(models.User, {as: 'Organizer', foreignKey: "organizerId" });
 
+      // }static membersAndImages() {
+      //   return {
+      //     attributes: {
+      //       include: [
+      //         'id','updatedAt','createdAt',
+      //         [sequelize.fn('COUNT', sequelize.col('Memberships.id')), 'numMembers'],
+      //         [sequelize.col('GroupImages.url'), 'previewImage']
+      //       ]
+      //     },
+      //     include: [
+      //       {
+      //         model: sequelize.models.Membership,
+      //         attributes: []
+      //       },
+      //       {
+      //         model: sequelize.models.GroupImage,
+      //         attributes: []
+      //       }
+      //     ],
+      //     group: ['Group.id', 'GroupImages.id']
+      //   };
     }
   }
-  Group.init({
-    organizerId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'Users',
-        key: 'id',
-      }
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        len: {
-          args: [1,60],
-          msg: 'Name must be 60 characters or less'
+  Group.init(
+    {
+      organizerId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: "id",
         },
-      }
-    },
-    about: {
-      type: DataTypes.TEXT,
-      validate: {
-        min:{
-          args: 50,
-          msg: 'About must be 50 characters or more'
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: {
+            args: [1, 60],
+            msg: "Name must be 60 characters or less",
+          },
         },
-      }
+      },
+      about: {
+        type: DataTypes.TEXT,
+        validate: {
+          min: {
+            args: 50,
+            msg: "About must be 50 characters or more",
+          },
+        },
+      },
+      type: {
+        type: DataTypes.ENUM("In Person", "Online"),
+        allowNull: false,
+        validate: {
+          notIn: {
+            args: ["In Person", "Online"],
+            msg: "Type must be 'Online' or 'In Person'",
+          },
+        },
+      },
+      private: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      city: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isAlpha: true,
+          min: 2,
+          max: 20,
+          isInPerson(val) {
+            if (this.type !== "Online" && !val) {
+              throw new Error("City is required for In Person events.");
+            }
+          },
+        },
+      },
+      state: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+          isAlpha: true,
+          min: 2,
+          max: 20,
+          isInPerson(val) {
+            if (this.type !== "Online" && !val) {
+              throw new Error("State is required for In Person events.");
+            }
+          },
+        },
+      },
     },
-    type: {
-      type: DataTypes.ENUM('In Person', 'Online'),
-      allowNull: false,
-      validate: {
-        notIn: {
-          args: ['In Person', 'Online'],
-          msg: "Type must be 'Online' or 'In Person'"
-        }
-      }
-    },
-    private: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-    },
-    city: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isAlpha: true,
-        min: 2,
-        max: 20,
-        isInPerson(val){
-          if (this.type !== 'Online' && !val) {
-            throw new Error('City is required for In Person events.');
-          }
-        }
-      }
-    },
-    state: {
-      type: DataTypes.STRING,
-      allowNull:true,
-      validate: {
-        isAlpha: true,
-        min: 2,
-        max: 20,
-        isInPerson(val){
-          if (this.type !== 'Online' && !val) {
-            throw new Error('State is required for In Person events.');
-          }
-        }
-      }
-    },
-  }, {
-    sequelize,
-    modelName: 'Group',
-    defaultScope:{
-      attributes: {
-        include:['id','updatedAt','createdAt', ]
-      }
+    {
+      sequelize,
+      modelName: "Group",
+      defaultScope: {
+        attributes: {
+          include: ["id", "updatedAt", "createdAt"],
+        },
+      },
+      // scopes: {
+      //   membersAndImages: {
+      //     attributes: {
+      //       include: [
+      //         "id",
+      //         "updatedAt",
+      //         "createdAt",
+      //         [
+      //           sequelize.fn("COUNT", sequelize.col("Memberships.id")),
+      //           "numMembers",
+      //         ],
+      //         [sequelize.col("GroupImages.url"), "previewImage"],
+      //       ],
+      //     },
+      //     include: [
+      //       {
+      //         model: sequelize.models.Membership,
+      //         attributes: [],
+      //       },
+      //       {
+      //         model: sequelize.models.GroupImage,
+      //         attributes: [],
+      //       },
+      //     ],
+      //     group: ["Group.id", "GroupImages.id"],
+      //   },
+      // },
     }
-  });
+  );
   return Group;
 };
--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -8,110 +8,138 @@ const {
   GroupImage,
 } = require("../../db/models");
 
+const { setTokenCookie, restoreUser } = require("../../utils/auth");
+
 const group = express.Router();
 
-                // FUNCTIONS
+// FUNCTIONS
 
 // ! Make func for numMembers/previewImage
 // * Counts members and include imagePrev
 
-const numMembers = () => {
-  return {
-    attributes: {
-      include: [
-        [Sequelize.fn("COUNT", Sequelize.col("Memberships.id")), "numMembers"],
-        [Sequelize.col("GroupImages.url"), "previewImage"],
-      ],
-    },
-    include: [
-      {
-        model: Membership,
-        attributes: [],
-      },
-      {
-        model: GroupImage,
-        attributes: [],
-      },
-    ],
-    group: ["Group.id", "GroupImages.id"],
-  };
-};
+// const numMembers = () => {
+//   return {
+//     attributes: {
+//       include: [
+//         [Sequelize.fn('COUNT', Sequelize.col('Memberships.id')), 'numMembers'],
+//         [Sequelize.col('GroupImages.url'), 'previewImage']
+//       ]
+//     },
+//     include: [
+//       {
+//         model: Membership,
+//         attributes: []
+//       },
+//       {
+//         model: GroupImage,
+//         attributes: []
+//       }
+//     ],
+//     group: ['Group.id', 'GroupImages.id']
+//   };
+// };
 
-// * include imagePrev
+// // * include imagePrev
 
-const prevImage = () => {
-  return {
-    attributes: {
-      include: [
-        [Sequelize.col("GroupImages.url"), "previewImage"],
-      ],
-    },
-    include: [
-      {
-        model: GroupImage,
-        attributes: [],
-      },
-    ],
-    group: ["Group.id", "GroupImages.id"],
-  };
-};
+// const prevImage = () => {
+//   return {
+//     attributes: {
+//       include: [
+//         [Sequelize.col('GroupImages.url'), 'previewImage']
+//       ]
+//     },
+//     include: [
+//       {
+//         model: GroupImage,
+//         attributes: []
+//       }
+//     ],
+//     group: ["Group.id", "GroupImages.id"],
+//   };
+// };
 
-const getCurrUser = (req,res) => {
-  const { user } = req;
-    if(user) {
-        const safeUser = {
-            id: user.id,
-            email: user.email,
-            username: user.username
-        };
-        return res.json({
-            user: safeUser
-        });
-    }else return res.json({ user: null });
-}
-
-
-                  //ROUTES
+//ROUTES
 
 // * Returns all Groups
 group.get("/", async (req, res) => {
-  const members = numMembers();
-  const imgs = prevImage();
-  const allGroups = await Group.findAll(members,imgs);
+  // const members = numMembers();
+  // const imgs = prevImage();
+  const allGroups = await Group
+    .findAll
+    // members// imgs,
+    ();
   return res.json(allGroups);
 });
 
-// ! find currUser
-// ? Returns all Groups currUser is a part of
+// * Returns all Groups currUser is a part of
 group.get("/current", async (req, res) => {
-  const members = numMembers();
-  const imgs = prevImage();
-  const currUser = getCurrUser();
+  // const members = numMembers();
+  // const imgs = prevImage();
+  const { user } = req;
 
-  if(currUser){
-    const groupOfCurrUser = await Membership.findByPk(currUser.id,{
-      include:
-    },members,imgs);
+  if (user) {
+    const groupOfCurrUser = await Group.findAll({
+      include: [
+        {
+          model: Membership,
+          attributes: [],
+          where: {
+            userId: user.id,
+            status: "member" || 'co-host',
+          },
+        },
+      ],
+    });
+    return res.json(groupOfCurrUser);
   }
-
-
-  
-  return res.json(groupOfCurrUser);
 });
 
 // * Returns all Group by it's ID
-
 group.get("/:groupId", async (req, res) => {
+  // const members = numMembers();
   const currGroupId = req.params.groupId;
-  const groupById = await Group.findOne({
-    where: {
-      id: currGroupId,
-    },
-  });
+  
+  if (!currGroupId) {
+    const organizer = currGroupId.organizerId;
+    const groupById = await Group.findOne({
+      where: {
+        id: currGroupId,
+      },
+      include: [
+        {
+          model: GroupImage,
+          attributes: {
+            exclude: ["groupId", "createdAt", "updatedAt"],
+          },
+        },
+        {
+          model: User,
+          as: "Organizer",
+          attributes: {
+            exclude: [
+              "username",
+              "email",
+              "hashedPassword",
+              "createdAt",
+              "updatedAt",
+            ],
+          },
+        },
+        {
+          model: Venue,
+          attributes: {
+            exclude: ["createdAt", "updatedAt"],
+          },
+        },
+      ],
+    });
 
-  return res.json(groupById);
+    return res.json(groupById);
+  }else {
+    res.status(404);
+    res.send({ message: "Group couldn't be found" })
+  }
 });
-
 
 // ! Creates and returns Group
 group.post("/", async (req, res) => {
@@ -130,8 +158,39 @@ group.post("/", async (req, res) => {
 });
 
 // ! Adds Img to Group based on ID
+group.post("/:groupId/images", async (req, res) => {
+  const { url, preview } = req.body;
+
+  const newImg = GroupImage.build({
+    groupId: this.group.id,
+    url: url,
+    preview: preview,
+  });
+
+  await newImg.save();
+});
+
 // ! Edits Group belonging to currUser
+
 // ! Deletes belonging to currUser
-// ! Returns all Groups
+// group.delete('/:groupId', async (req,res) => {
+
+// })
+
+// * Returns all Venues for Group by ID
+group.get("/:groupId/venues", async (req, res) => {
+  const id = req.params.groupId;
+  const venuesOfGroup = await Venue.findAll({
+    where: {
+      groupId: id,
+    },
+    attributes: {
+      exclude: ["createdAt", "updatedAt"],
+    },
+  });
+  return res.json(venuesOfGroup);
+});
+// ! Create newVenue for Group by ID
+// ! Edit Venue by ID
 
 module.exports = group;
