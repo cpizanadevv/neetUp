1eb881693ac698146c4ca5000e0851e97e76cdd4
cj <c.pizana.devv@gmail.com>
2024-06-11 12:48:10 -0700

    clg

--- backend/db/migrations/20240406211154-create-venue.js
+++ backend/db/migrations/20240406211154-create-venue.js
@@ -53,9 +53,8 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    }, options);
-  
-    console.log("Migration successful")
+    }, options,
+    console.log("Migration successful"));
   },
   async down(queryInterface, Sequelize) {
     options.tableName = "Venues";
