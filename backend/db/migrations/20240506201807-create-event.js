87767fd945a8fb37a9e42b318fa62e4e1b6dd528
cj <c.pizana.devv@gmail.com>
2024-06-12 12:38:49 -0700

    logging

--- backend/db/migrations/20240506201807-create-event.js
+++ backend/db/migrations/20240506201807-create-event.js
@@ -4,6 +4,7 @@ let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
+console.log(`Using schema: ${schema}`);
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Events', {
