011c3aa3bc937400004354e3a2178af7a598798e
cj <c.pizana.devv@gmail.com>
2024-06-12 12:44:15 -0700

    logging

--- backend/db/seeders/20240509223530-demo-event.js
+++ backend/db/seeders/20240509223530-demo-event.js
@@ -7,6 +7,7 @@ let options = {};
 
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
+  console.log(`Using schema: ${options.schema}`);
 }
 module.exports = {
   async up(queryInterface, Sequelize) {
