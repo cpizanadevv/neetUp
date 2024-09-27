fc33f4d6308e440a96a4554637c2117b89253847
cj <c.pizana.devv@gmail.com>
2024-09-27 16:39:14 -0700

    removed a few seeders

--- backend/db/seeders/20240510204638-demo-membership.js
+++ backend/db/seeders/20240510204638-demo-membership.js
@@ -68,45 +68,6 @@ module.exports = {
       { userId: 23, groupId: 10, status: "member" },
       { userId: 26, groupId: 10, status: "pending" },
 
-      // Memberships for "Anime Fitness Squad"
-      { userId: 26, groupId: 11, status: "co-host" },
-      { userId: 27, groupId: 11, status: "member" },
-      { userId: 28, groupId: 11, status: "pending" },
-
-      // Memberships for "Seinen Society"
-      { userId: 27, groupId: 12, status: "co-host" },
-      { userId: 29, groupId: 12, status: "member" },
-      { userId: 30, groupId: 12, status: "pending" },
-
-      // Memberships for "Sports Anime Enthusiasts"
-      { userId: 28, groupId: 13, status: "co-host" },
-      { userId: 31, groupId: 13, status: "member" },
-      { userId: 32, groupId: 13, status: "pending" },
-
-      // Memberships for "Historical Anime Group"
-      { userId: 29, groupId: 14, status: "co-host" },
-      { userId: 33, groupId: 14, status: "member" },
-      { userId: 34, groupId: 14, status: "pending" },
-
-      // Memberships for "Fantasy Worlds Club"
-      { userId: 30, groupId: 15, status: "co-host" },
-      { userId: 35, groupId: 15, status: "member" },
-      { userId: 36, groupId: 15, status: "pending" },
-
-      // Memberships for "Anime Cooking Club"
-      { userId: 31, groupId: 16, status: "co-host" },
-      { userId: 37, groupId: 16, status: "member" },
-      { userId: 38, groupId: 16, status: "pending" },
-
-      // Memberships for "Anime Book Club"
-      { userId: 32, groupId: 17, status: "co-host" },
-      { userId: 39, groupId: 17, status: "member" },
-      { userId: 40, groupId: 17, status: "pending" },
-
-      // Memberships for "Miyazaki Appreciation Society"
-      { userId: 33, groupId: 18, status: "co-host" },
-      { userId: 4, groupId: 18, status: "member" },
-      { userId: 2, groupId: 18, status: "pending" }
     ], { validate: true });
   },
 
