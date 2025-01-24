f093d230dd93db820612f54b735f3e5fd83f7a4e
cj <c.pizana.devv@gmail.com>
2025-01-24 15:05:01 -0800

    login modal done

--- frontend/src/components/LoginFormModal/LoginFormModal.jsx
+++ frontend/src/components/LoginFormModal/LoginFormModal.jsx
@@ -41,36 +41,39 @@ function LoginFormModal() {
   };
 
   return (
-    <>
-      <h1>Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
+    <div className="flex flex-col justify-center items-center bg-white p-12 w-96">
+      <h1 className="text-2xl font-semibold">Log In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col items-center w-2/3 mt-4">
+        <label  className="flex flex-col w-full text-center ">
+          Email
           <input
             type="text"
-            placeholder="
-            Username or Email"
             value={credential}
             onChange={(e) => setCredential(e.target.value)}
             required
+            className="border border-black"
+          
           />
-        </div>
-        <div>
+        </label>
+        <label  className="flex flex-col w-full text-center ">
+          Password
           <input
             type="password"
-            placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
+            className="border border-black"
+          
           />
 
           {errors.credential && <p>{errors.credential}</p>}
-        </div>
+        </label>
 
-        <button type="submit">Log In</button>
+        <button type="submit" className="hover:bg-blue-950 hover:text-white w-32 p-2">Log In</button>
 
       </form>
-      <button onClick={handleDemoSubmit}>Log in as Demo User</button>
-    </>
+      <button onClick={handleDemoSubmit}className="hover:bg-blue-950 hover:text-white w-32 p-2">Demo User</button>
+    </div>
   );
 }
 
