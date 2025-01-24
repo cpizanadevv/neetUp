04294f1a0551f30d3d3bb6be7f7a4dbe1e931310
cj <c.pizana.devv@gmail.com>
2025-01-24 14:24:27 -0800

    adding tailwind to modals

--- frontend/src/components/SignupFormModal/SignupFormModal.jsx
+++ frontend/src/components/SignupFormModal/SignupFormModal.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import "./SignupForm.css";
 import * as sessionActions from "../../store/session";
 import { useModal } from "../../context/Modal";
 
@@ -43,74 +42,80 @@ function SignupFormModal() {
   };
 
   return (
-    <>
-      <h1>Sign Up</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
+    <div className="flex flex-col justify-center items-center bg-white p-12 w-96">
+      <h1 className="text-2xl font-semibold">Sign Up</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col items-center w-2/3 mt-4">
+        <label className="flex flex-col w-full text-center ">
           Email
           <input
             type="text"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
+            className="border border-black"
           />
         </label>
         {errors.email && <p className="err">{errors.email}</p>}
-        <label>
+        <label className="flex flex-col w-full text-center ">
           Username
           <input
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
             required
+            className="border border-black"
           />
         </label>
         {errors.username && <p className="err">{errors.username}</p>}
-        <label>
+        <label className="flex flex-col w-full text-center ">
           First Name
           <input
             type="text"
             value={firstName}
             onChange={(e) => setFirstName(e.target.value)}
             required
+            className="border border-black"
           />
         </label>
         {errors.firstName && <p className="err">{errors.firstName}</p>}
-        <label>
+        <label className="flex flex-col w-full text-center ">
           Last Name
           <input
             type="text"
             value={lastName}
             onChange={(e) => setLastName(e.target.value)}
             required
+            className="border border-black"
           />
         </label>
         {errors.lastName && <p className="err">{errors.lastName}</p>}
-        <label>
+        <label className="flex flex-col w-full text-center ">
           Password
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
+            className="border border-black"
           />
         </label>
         {errors.password && <p className="err">{errors.password}</p>}
-        <label>
+        <label className="flex flex-col w-full text-center ">
           Confirm Password
           <input
             type="password"
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
             required
+            className="border border-black"
           />
         </label>
         {errors.confirmPassword && (
           <p className="err">{errors.confirmPassword}</p>
         )}
-        <button type="submit" disabled={username.length < 4 || password.length < 6}>Sign Up</button>
+        <button type="submit" disabled={username.length < 4 || password.length < 6} className="hover:bg-blue-950 hover:text-white w-32">Sign Up</button>
       </form>
-    </>
+    </div>
   );
 }
 
